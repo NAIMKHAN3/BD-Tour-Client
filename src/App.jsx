d6a5520c4d980f52405603c694b00f864bb87c9b
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-11-10 06:44:39 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Router/Router'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
   return (
     <>
+    <Toaster/>
     <RouterProvider router={router} />
     </>
   )
--- src/components/DestinationCard.jsx
+++ src/components/DestinationCard.jsx
@@ -4,9 +4,9 @@ import {BsArrowRightCircle} from 'react-icons/bs'
 const DestinationCard = () => {
     return (
         <div className='border border-secondary p-3 rounded-md'>
-            <img className='h-36 w-full' src="https://i.ibb.co/1nBnSTD/jaflong.jpg" alt="" />
+            <img className='h-52 w-full' src="https://i.ibb.co/1nBnSTD/jaflong.jpg" alt="" />
 
-           <div className='flex justify-between items-center mt-2'>
+           <div className='flex justify-between items-center my-5'>
            <h1 className='font-semibold'>Dhaka</h1>
             <BsArrowRightCircle className='text-secondary hover:bg-secondary hover:text-white rounded-full cursor-pointer duration-300' size={24} />
            </div>
